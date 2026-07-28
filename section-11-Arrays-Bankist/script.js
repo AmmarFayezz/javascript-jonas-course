2e70b0fbdf929f6cba0d5fcc72a395f5e6062a1a
Ammar Fayez <ammarfayez858@gmail.com>
2026-07-29 02:43:43 +0300

    Section 11 flat && flatMap methods

--- section-11-Arrays-Bankist/script.js
+++ section-11-Arrays-Bankist/script.js
@@ -641,3 +641,30 @@ const deposit = (mov) => mov > 0;
 console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
+
+//flat method
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat()); //default 1
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+//==========================================================
+
+// const accountMovements = accounts.map((acc) => acc.movements);
+// console.log(accountMovements);
+// const totalMovements = accountMovements.flat();
+// console.log(totalMovements);
+// const overallBalance = totalMovements.reduce((acc, mov) => acc + mov);
+// console.log(overallBalance);
+
+const overallBalance = accounts
+  .map((acc) => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov);
+console.log(overallBalance);
+
+const overallBalance2 = accounts
+  .flatMap((acc) => acc.movements)
+  .reduce((acc, mov) => acc + mov);
+console.log(overallBalance2);
